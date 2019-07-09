ff9545cec76ecf1153c9325fd81dfa3e8bba6c13
Andrija Perovic <andrija989@gmail.com>
2019-07-09 20:44:03 +0200

    Zavrsen rad

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -12,9 +12,13 @@ class CarController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Car::all();
+        $skip = request()->input('skip', 0);
+        $take = request()->input('take', Car::get()->count());
+        
+        return Car::skip($skip)->take($take)->get();
+        
     }
 
     /**
